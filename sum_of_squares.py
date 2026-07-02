c7fe8e56a03755bbafe52a2b519158e63cdf06ac
giri-codes672 <giribabu9059@gmail.com>
2026-07-02 12:29:17 +0530

    Create sum_of_squares.py

---
+++ sum_of_squares.py
@@ -0,0 +1,11 @@
+# Sum of Squares using for loop
+
+numbers = [1, 2, 3, 4, 5]
+
+sum_of_squares = 0
+
+for num in numbers:
+    sum_of_squares = sum_of_squares + (num * num)
+
+print("List:", numbers)
+print("Sum of Squares =", sum_of_squares)
